c02ded3a377c43a9dc040cfb6befb44fa5a33639
Alex Andrade <alexander.andrade@gmail.com>
2025-03-28 21:43:58 -0500

    ♻️ improve environment questions

--- src/test/java/com/nuvei/screenplay/questions/EnvironmentQuestion.java
+++ src/test/java/com/nuvei/screenplay/questions/EnvironmentQuestion.java
@@ -27,16 +27,25 @@ public class EnvironmentQuestion {
 
     public static Question<Boolean> isTSeries() {
         // T series has 2 letters like "us", "de", "fr", etc.
-        return actor -> environmentVariables.getProperty("environment").length() == 2;
+        return actor -> actor.asksFor(getVariable("environment")).length() == 2;
     }
 
     public static Question<Boolean> isMSeries() {
-        String environmentVariable = environmentVariables.getProperty("environment");
-        return actor -> environmentVariable.startsWith("m") && environmentVariable.length() == 3;
+        return actor -> {
+            String environmentVariable = actor.asksFor(getVariable("environment"));
+            return environmentVariable.startsWith("m") && environmentVariable.length() == 3;
+        };
     }
 
     public static Question<Boolean> isPSeries() {
-        String environmentVariable = environmentVariables.getProperty("environment");
-        return actor -> environmentVariable.startsWith("p") && environmentVariable.length() == 3;
+        return actor -> {
+            String environmentVariable = actor.asksFor(getVariable("environment"));
+            return environmentVariable.startsWith("p") && environmentVariable.length() == 3;
+        };
+    }
+
+    public static Question<String> getVariable(String variableName) {
+        String environmentVariable = environmentVariables.getProperty(variableName);
+        return actor -> environmentVariable;
     }
 }
